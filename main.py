e629f535bc57ffdcf569befcd258552b7a55fbf0
marcenoandrei <marcenoandrei24@gmail.com>
2023-11-22 17:27:01 +0800

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import base64
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+credentials = ["marcenoandrei24@gmail.com", "p@ssweord"]
+base64_credentials = base64.b64encode("".join(credentials).encode()).decode()
+print('Authorization : basic' + base64_credentials)
